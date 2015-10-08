37b595f5b9b78c3ebc4cae7504076999a2bb061e
Joaquin <jodaheca@jodaheca-VirtualBox.(none)>
2015-10-07 23:49:07 -0500

    pruebas en heroku

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name spinnerBankAngularApp
+ * @description
+ * # spinnerBankAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('spinnerBankAngularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
