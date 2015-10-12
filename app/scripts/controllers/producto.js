157ffd07b237eb0b6a199b4d3d1ba66f69488e0b
johnba <johnbairon.londo45@gmail.com>
2015-10-12 05:50:45 -0500

    implementacion del controlador producto

--- app/scripts/controllers/producto.js
+++ app/scripts/controllers/producto.js
@@ -5,13 +5,25 @@
  * @name spinnerBankAngularApp.controller:prodControler
  * @description
  * # prodControler
- * Controller of the spinnerBankAngularApp
+ * Controller of the spinnerBank
  */
-angular.module('spinnerBankAngularApp')
-  .controller('prodControler', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+spinnerBankAngularApp.controller('prodControler', function ($scope, productWebService, shared) {
+	$scope.userIdNumber=1936941186;
+	productWebService.products($scope.userIdNumber).success(
+				function(data) {
+					selectedProduct = undefined;
+					$scope.products = data;
+
+			});
+
+			$scope.productView = function() {
+				if (typeof ($scope.selectedProduct) == 'undefined'
+						|| angular.equals($scope.selectedProduct, "undefined")) {
+					alert("Por favor seleccione un producto");
+					return;
+				}
+
+				shared.selectedProduct($scope.selectedProduct);
+				$location.url("/views/detalles");
+				};
   });
