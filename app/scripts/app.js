a3fadb60fe5b792bab5b41f2bf2efbd60178d82a
johnba <johnbairon.londo45@gmail.com>
2015-10-12 06:09:53 -0500

    implementacion de controlador de vista producto

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+
+/**
+ * Variable que expone la ruta al Servicio Web que permite obtener los diferentes  productos que tiene un cliente con el banco.
+ */
+var PRODUCT_WEB_SERVICE = 'https://spinnerbank-api-external.herokuapp.com/productos/';
 /**
  * @ngdoc overview
  * @name spinnerBankAngularApp
@@ -8,8 +13,7 @@
  *
  * Main module of the application.
  */
-angular
-  .module('spinnerBankAngularApp', [
+var spinnerBankAngularApp= angular.module('spinnerBankAngularApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
@@ -33,3 +37,28 @@ angular
         redirectTo: '/'
       });
   });
+
+/**
+ * Servicio empleado para hacer uso del Servicio Web que permite
+ * obtener el listado de productos del cliente identificado con userIdNumber
+ */
+    spinnerBankAngularApp.service('productWebService', function($http) {
+    this.products = function(userIdNumber) {  
+      return ($http({
+        method : 'GET',
+        url : PRODUCT_WEB_SERVICE+userIdNumber
+      }));
+    };
+  });
+
+spinnerBankAngularApp.service('shared', function() {
+  var product= {};
+  return {
+    getProduct : function() {
+      return product;
+    },
+    setProduct : function(value) {
+      product = value;
+    }
+  };
+});
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spinnerbankAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the spinnerbankAngularApp
+ */
+angular.module('spinnerbankAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
