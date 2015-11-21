111ff0317d5302986c5c8d6e2668d96175a31dc9
Joaquin Hernandez <jdavidhc94@gmail.com>
2015-11-20 22:06:53 -0500

    Agrego los servicio web necesarios para obtener el token de acceso a google desde el Api Backend y la informacion del usuario

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -23,7 +23,7 @@ angular.module('productos.services', [])
         return $http.get('https://accounts.google.com/o/oauth2/auth', {
           method: 'GET',
           headers : {
-            'Access-Control-Allow-Origin' : '*'
+            'Access-Control-Allow-Origin' : 'http://localhost:9000'
           },
           params : {
             'response_type':'code',
@@ -33,6 +33,46 @@ angular.module('productos.services', [])
             'state':'security_token',
             'access_type':'offline',
             'approval_prompt':'force'
+          }
+        });
+      },
+
+      loginGoogle2: function() {
+        return $http.jsonp('https://accounts.google.com/o/oauth2/auth', {
+          method: 'jsonp',
+          callback: "JSON_CALLBACK",
+          format:'jsonp',
+          dataType: 'jsonp',
+          params : {
+            'response_type':'code',
+            'client_id':client_id,
+            'redirect_uri':'http://spinnerbank-api-external.herokuapp.com/v1/oAuth2/code',
+            'scope':'email',
+            'state':'security_token',
+            'access_type':'offline',
+            'approval_prompt':'force'
+          }
+        });
+      },
+
+      //Servicio mediante el cual se envia el token de seguridad a SpinnerBank Backend para 
+      // que desde este se realice la autenticacion del usuario
+      obtenerTokenApi: function(codigoGoogle) {
+        return $http.get('http://spinnerbank-api-external.herokuapp.com/v1/oAuth2/accessToken', {
+          method: 'get',
+          params : {
+            'code':codigoGoogle
+          }
+        });
+      },
+
+      //Servicio encargado de obtener la informacion del usuario logeado en el 
+      // sistema mediante el Token de acceso que provee el Api Backend
+      obtenerInfoUsuario: function(TokenApi) {
+        return $http.get('http://spinnerbank-api-external.herokuapp.com/v1/oAuth2/userInfo', {
+          method: 'get',
+          params : {
+            'access_token':TokenApi
           }
         });
       }
