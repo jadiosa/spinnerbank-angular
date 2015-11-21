368712273d036875b48006eb410e7058d33e1776
Joaquin Hernandez <jdavidhc94@gmail.com>
2015-11-20 22:04:04 -0500

    Implementacion de un nuevo metodo para login con Google

--- app/scripts/LoginCtrl.js
+++ app/scripts/LoginCtrl.js
@@ -2,9 +2,9 @@
 
   .controller('LoginCtrl', function($scope, $location, $auth, API, toastr) {
    
-    $scope.login = function() {
+    $scope.login2 = function() {
         
-      API.loginGoogle()
+      API.loginGoogle2()
       .success(function(data) {
         toastr.success('Logeo exitoso');
         console.log('Login Correcto: ',data);
@@ -12,6 +12,21 @@
       }).error(function (data, status) {
           console.log('Error: ',data)
         })
+    };
+
+    $scope.login = function() {
+        var scope = 'email';
+        var client_id = '116421120632-otf7afrfqtfeiqlibtlatnou8964bge0.apps.googleusercontent.com';
+        var redirect_uri = 'http://spinnerbank-api-external.herokuapp.com/v1/oAuth2/code';
+        var response_type = 'code';
+        var state = 'security_token';
+        var access_type = 'offline';
+        var approval_prompt = 'force';
+        var url="https://accounts.google.com/o/oauth2/auth?scope="+
+        scope+"&client_id="+client_id+"&redirect_uri="+redirect_uri+
+       "&response_type="+response_type+"&state="+state+"&access_type="+
+        access_type+"&approval_prompt="+approval_prompt;
+        window.location.replace(url);
     };
 
     $scope.authenticate = function(provider) {
