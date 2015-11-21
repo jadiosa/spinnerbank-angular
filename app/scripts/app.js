5d6f77e764c495070e0851957a9354b04d9d1229
Joaquin Hernandez <jdavidhc94@gmail.com>
2015-11-20 22:05:20 -0500

    Cambios en el app.js para redireccionar la la vista cuando se hace la primera peticion a google

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -41,6 +41,31 @@ var spinnerBankAngularApp = angular.module('spinnerBankAngularApp', [
         controller: 'prodControler',
         controllerAs: 'Producto'
       })
+
+      .when('/llamarApi', {
+        templateUrl: 'views/Producto.html',
+        controller: 'SecurityCtrl',
+        controllerAs: 'SecurityCtrl'
+      })
+
+      .when('/access_token=:accessToken', {
+          template: '',
+          controller: function ($location,$rootScope) {
+            var hash = $location.path().substr(1);
+           
+           var splitted = hash.split('&');
+           var params = {};
+ 
+           for (var i = 0; i < splitted.length; i++) {
+             var param  = splitted[i].split('=');
+             var key    = param[0];
+             var value  = param[1];
+             params[key] = value;
+             $rootScope.accesstoken=params;
+           }
+           $location.path("/llamarApi");
+         }
+       })
       .otherwise({
         redirectTo: '/'
       });
